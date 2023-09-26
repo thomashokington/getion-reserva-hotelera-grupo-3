ca25d7076df7134a0b738f5b8ef3621247c7c820
Alonsont <114254151+Alonsont@users.noreply.github.com>
2023-09-26 00:21:50 -0300

    programación del backend para la busqueda de habitaciones

---
+++ Rhotel/reservas/models.py
@@ -0,0 +1,82 @@
+
+
+from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+class Hotel(models.Model):
+    nombre = models.CharField(max_length=255)
+    ubicacion = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    estrellas = models.IntegerField()
+
+    # ...
+
+class TipoDeHabitacion(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    precio_base = models.DecimalField(max_digits=10, decimal_places=2)
+    capacidad = models.IntegerField()
+
+    # ...
+
+class Habitacion(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    tipo_habitacion = models.ForeignKey(TipoDeHabitacion, on_delete=models.CASCADE)
+    numero = models.CharField(max_length=20)
+    # Elimina los campos fecha_disponible_inicio y fecha_disponible_fin
+    disponible = models.BooleanField(default=True)  # Cambia esto a un campo booleano para disponibilidad
+
+    # ...
+
+
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    correo = models.EmailField(max_length=255)
+    telefono = models.CharField(max_length=20)
+
+    # ...
+class Reserva(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE)
+    fecha_entrada = models.DateField()
+    fecha_salida = models.DateField()
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+@receiver(pre_save, sender=Reserva)
+def actualizar_disponibilidad(sender, instance, **kwargs):
+    habitacion = instance.habitacion
+    fecha_entrada = instance.fecha_entrada
+    fecha_salida = instance.fecha_salida
+
+    if habitacion and fecha_entrada and fecha_salida:
+        # Obtén todas las reservas que se superponen con las fechas de la nueva reserva
+        reservas_superpuestas = Reserva.objects.filter(
+            habitacion=habitacion,
+            fecha_entrada__lte=fecha_salida,
+            fecha_salida__gte=fecha_entrada
+        )
+
+        # Si hay reservas superpuestas, marca la habitación como no disponible
+        if reservas_superpuestas.exists():
+            habitacion.disponible = False
+        else:
+            # Si no hay reservas superpuestas, marca la habitación como disponible
+            habitacion.disponible = True
+
+        # Guarda los cambios en la disponibilidad de la habitación
+        habitacion.save()
+
+
+@receiver(pre_save, sender=Reserva)
+def calcular_precio_total(sender, instance, **kwargs):
+    habitacion = instance.habitacion
+    fecha_entrada = instance.fecha_entrada
+    fecha_salida = instance.fecha_salida
+    if habitacion and fecha_entrada and fecha_salida:
+       
+        precio_total = calcular_precio_total(fecha_entrada, fecha_salida, habitacion.precio_base)
+        instance.precio_total = precio_total
